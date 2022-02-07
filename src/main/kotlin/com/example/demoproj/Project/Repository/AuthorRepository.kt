81bcb55d9614420dd5cec322eff4dcf77072f102
SaketVajpai <51900856+SaketVajpai@users.noreply.github.com>
2022-02-07 17:01:54 +0530

    Delete src/main/kotlin/com/example/demoproj/Project/Repository directory

--- src/main/kotlin/com/example/demoproj/Project/Repository/AuthorRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demoproj.Project.Repository
-
-import com.example.demoproj.Project.entity.Author
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AuthorRepository: JpaRepository<Author,Int>{
-    fun findAuthor(author: String): Author
-}
